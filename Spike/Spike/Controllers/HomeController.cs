76c9a0c30eb460ba7b3452b9286c35c60da974bb
John Hilts <John Hilts>
2015-12-31 19:19:51 -0800

    added simple weather

--- Spike/Spike/Controllers/HomeController.cs
+++ Spike/Spike/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
 
 namespace Spike.Controllers
 {
@@ -10,9 +14,33 @@ namespace Spike.Controllers
         }
 
         [HttpGet]
-        public JsonResult GetWeather()
+        public JsonResult GetWeather(decimal latitude, decimal longitude)
         {
-            return Json(new { Result = true }, JsonRequestBehavior.AllowGet);
+            return Json(GetWeatherInfo(latitude, longitude), JsonRequestBehavior.AllowGet);
+        }
+
+        private WeatherInfo GetWeatherInfo(decimal latitude, decimal longitude)
+        {
+            var jsonString = GetWeatherInfoApi(latitude, longitude);
+            var weatherData = JsonConvert.DeserializeObject<WeatherInfo>(jsonString);
+
+            if (weatherData == null || weatherData.Weather == null || weatherData.Weather[0].Description == null || weatherData.Main.Temp == null)
+            {
+                throw new System.Exception("No valid data retrieved");
+            }
+
+            return weatherData;
+        }
+
+        private string GetWeatherInfoApi(decimal latitude, decimal longitude)
+        {
+            string apiKey = System.IO.File.ReadAllText(Server.MapPath("/env/key.txt")); 
+
+            string openWeatherMapApiFormat = "http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&appid={2}";
+            var client = new WebClient();
+            var response = client.DownloadString(string.Format(openWeatherMapApiFormat, latitude, longitude, apiKey));
+            return response;
+
         }
 
         public ActionResult About()
@@ -27,6 +55,27 @@ namespace Spike.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+    }
+
+    public class WeatherInfo
+    {
+        public List<weather> Weather { get; set; }
+        public main Main { get; set; }
+        public WeatherInfo()
+        {
+            this.Weather = new List<weather>();
+            this.Main = new main();
+        }
+
+        public class main
+        {
+            public string Temp { get; set; }
+        }
+
+        public class weather
+        {
+            public string Description { get; set; }
         }
     }
 }
