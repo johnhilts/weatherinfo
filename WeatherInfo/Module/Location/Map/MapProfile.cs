cc7ed9e03dc4a7990450e49a63df9550c351071c
John Hilts <johnhilts@live.com>
2016-01-03 15:12:34 -0800

    added auto mapper

---
+++ WeatherInfo/Module/Location/Map/MapProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Location.Weather;
+
+namespace Location.Map
+{
+    public class MapProfile : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<WeatherDataModel, WeatherInfoModel>()
+                .ForMember(d => d.Temperature, o => o.MapFrom(source => source.Main.Temp));
+        }
+    }
+}
