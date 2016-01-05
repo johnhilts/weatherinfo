704535ae5463a0b02247bb76d68996e209beeab7
John Hilts <johnhilts@live.com>
2016-01-04 21:31:41 -0800

    initial commit for weather web site

---
+++ WeatherInfo/Web/WeatherInfo.Web/Controllers/WeatherController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WeatherInfo.Web.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+    }
+}
