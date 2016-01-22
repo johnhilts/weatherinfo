c33eb38ac00760399430fd26a656533446143412
John Hilts <johnhilts@live.com>
2016-01-21 23:26:57 -0800

    replaced weather helper JS class with angular weather service

--- WeatherInfo/Web/WeatherInfo.Web/js/main/WeatherController.js
+++ WeatherInfo/Web/WeatherInfo.Web/js/main/WeatherController.js
@@ -1,5 +1,5 @@
 ﻿angular.module("weather-main", [])
-    .controller('weatherController', function ($scope, locationService) {
+    .controller('weatherController', function ($scope, locationService, weatherService) {
         // TODO: get stuff from the server on page-load
 
         // TODO: pass a list of lat/long's from the server
@@ -15,10 +15,12 @@
                 .then(
                     function () {
                         $scope.errorMessages = locationService.errorMessages;
-                        $scope.setLocation(locationService.locationData.pop());
+                        var locationData = locationService.locationData.pop();
+                        $scope.setLocation(locationData);
+                        $scope.getCurrentWeather(locationData);
                     },
                     function () {
-                        alert("failed");
+                        alert("location failed");
                     }
                 );
 
@@ -28,13 +30,33 @@
             $scope.city = locationData.city;
             $scope.state = ", " + locationData.stateCode;
             $scope.country = locationData.countryCode;
+        }
 
-            weatherHelper = new weatherInfo.weatherHelper(locationData.latitude, locationData.longitude);
-            weatherHelper.getCurrentWeather();
+        $scope.getCurrentWeather = function (locationData) {
+
+            $scope.errorMessages = [];
+
+            weatherService.getCurrentWeather(locationData.latitude, locationData.longitude)
+                .then(
+                    function () {
+                        $scope.errorMessages = weatherService.errorMessages;
+                        var weatherData = weatherService.weatherData.pop();
+                        $scope.setWeather(weatherData);
+                    },
+                    function () {
+                        alert("weather failed");
+                    }
+                );
+
+        };
+
+        $scope.setWeather = function (weatherData) {
+            $scope.currentLocationTemperature = weatherData.currentTemperature;
+            $scope.currentLocationUnitType = weatherData.currentUnitType;
         }
 
         angular.element(document).ready(function () {
-            
+
             $scope.getCurrentLocation();
 
         });
---
+++ WeatherInfo/Web/WeatherInfo.Web/js/main/WeatherService.js
@@ -0,0 +1,36 @@
+﻿var weatherModule = angular.module("weather-main");
+
+weatherModule.factory("weatherService", function ($http, $q) {
+    
+    var _weatherData = [];
+    var _errorMessages = [];
+
+    var _getCurrentWeather = function (latitude, longitude) {
+        var d = $q.defer();
+
+        $http({
+            method: 'GET',
+            url: '/api/Weather',
+            params: { latitude: latitude, longitude: longitude, },  // pass in data as strings
+        })
+        .then(function (r) { _getGetCurrentWeatherSuccess(r); d.resolve(); }, function () { d.reject(); });
+
+        return d.promise;
+    };
+
+    var _getGetCurrentWeatherSuccess = function (result) {
+            if (result) {
+                var weatherData = {
+                    currentTemperature: currentTemperature = result.data.MainItems[0].CurrentTemperature,
+                    currentUnitType: result.data.UnitType,
+                };
+                _weatherData.push(weatherData);
+            }
+        };
+
+    return {
+        weatherData: _weatherData,
+        getCurrentWeather: _getCurrentWeather,
+        errorMessages: _errorMessages,
+    };
+});
