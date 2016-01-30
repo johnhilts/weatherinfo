8a41d64f7be3a3100df09a6c5b317550da29e38c
John Hilts <johnhilts@live.com>
2016-01-30 00:08:57 -0800

    hook up REST method to real weather service

--- WeatherInfo/Web/WeatherInfo.Web/ApiControllers/WeatherController.cs
+++ WeatherInfo/Web/WeatherInfo.Web/ApiControllers/WeatherController.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using Location.Weather.OpenWeatherMap.api;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 using WeatherInfo.Web.Models.Weather;
 
@@ -13,14 +9,20 @@ namespace WeatherInfo.Web.ApiControllers
     {
         public MainViewModel Get(decimal latitude, decimal longitude)
         {
+
+            var weatherService = new OpenWeatherMapWeatherService(System.Web.Hosting.HostingEnvironment.MapPath("/env/openWeatherMapKey.txt"));
+            var currentWeather = weatherService.GetCurrentWeatherByLocation(latitude, longitude);
+            currentWeather.UnitType = Location.Weather.UnitType.Fahrenheit;
+            currentWeather.Temperature = currentWeather.Temperature * 9 / 5 - 459.67m;
+
             var model = new MainViewModel
             {
                 MainItems =
                     new List<MainItemViewModel>
                     {
-                        new MainItemViewModel{ CityName="Glendale, CA, USA", CurrentTemperature=62, },
+                        new MainItemViewModel{ CityName="Glendale, CA, USA", CurrentTemperature= decimal.Round(currentWeather.Temperature, 1), },
                     },
-                UnitType = "F",
+                UnitType = currentWeather.UnitType.ToString("g").Substring(0, 1).ToUpper(),
             };
 
             return model;
