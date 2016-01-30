57ec8328ee0518decd3249ffbf6069635bab512c
John Hilts <johnhilts@live.com>
2016-01-30 00:05:46 -0800

    added automapper to web app

--- WeatherInfo/Web/WeatherInfo.Web/Global.asax.cs
+++ WeatherInfo/Web/WeatherInfo.Web/Global.asax.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Location.Map;
 
 namespace WeatherInfo.Web
 {
@@ -18,6 +16,8 @@ namespace WeatherInfo.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Mapper.Initialize(x => x.AddProfile<MapProfile>());
         }
     }
 }
