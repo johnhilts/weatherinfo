ef9f95a8c1dc2c14e6de1fc5ff94dca3fb2d3071
John Hilts <johnhilts@live.com>
2016-02-06 01:33:46 -0800

    SQL - new tables

---
+++ WeatherInfo/UserInfo.create.sql
@@ -0,0 +1,25 @@
+﻿use WeatherInfo
+-- drop TABLE dbo.UserInfo
+go
+/****** Object:  Table WeatherInfo.dbo].UserInfo    Script Date: 12/21/2014 6:46:24 PM ******/
+set ansi_nulls on
+go
+set quoted_identifier on
+go
+create table dbo.UserInfo (
+	UserId uniqueidentifier not null,
+	FirstName nvarchar(50) not null, 
+	LastName nvarchar(50) not null, 
+	CreatedDate smalldatetime not null,
+	IsActive bit not null constraint df_WeatherInfo_IsActive default 1,
+ constraint pk_WeatherInfo primary key clustered 
+(
+	UserId ASC
+)with (pad_index = off, statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) on [primary]
+) on [primary]
+
+insert into dbo.UserInfo (UserId, FirstName, LastName, CreatedDate, IsActive)
+values (newid(), 'Default', 'User', getdate(), 1)
+
+go
+
---
+++ WeatherInfo/UserLocations.create.sql
@@ -0,0 +1,25 @@
+﻿use WeatherInfo
+-- drop TABLE dbo.UserLocations
+go
+/****** Object:  Table dbo.UserLocations    Script Date: 12/21/2014 6:46:24 PM ******/
+set ansi_nulls on
+go
+set quoted_identifier on
+go
+create table dbo.UserLocations(
+	UserId uniqueidentifier not null,
+	GeoLocation geography not null,
+	City nvarchar(50) not null, 
+	StateCode varchar(2) not null, 
+	CountryCode varchar(2) not null, 
+	SortOrder tinyint not null, 
+ constraint [pk_UserLocations] primary key clustered 
+(
+	UserId asc
+)with (pad_index = off, statistics_norecompute = off, ignore_dup_key = off, allow_row_locks = on, allow_page_locks = on) on [primary]
+) on [primary]
+go
+
+alter table dbo.UserLocations  with check add  constraint FK_UserLocations_UserInfo foreign key(UserId)
+references dbo.UserInfo (UserId)
+go
