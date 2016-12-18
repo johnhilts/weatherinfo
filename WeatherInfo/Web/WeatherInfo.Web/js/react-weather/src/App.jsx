b1472e8ff7ac8ed51d579cf752e77c47dcd359c8
John Hilts <johnhilts@live.com>
2016-12-18 13:12:41 -0800

    switched to jsx to accommodate VS2015
    needed to update create-react-app
    then it all worked no problem
    reactjs team, however, doesn't recommend using the jsx extension

--- WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/App.jsx
+++ WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/App.jsx
@@ -13,6 +13,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Using the JSX extension now!</p>
+        <p>1 more test</p>
       </div>
     );
   }
