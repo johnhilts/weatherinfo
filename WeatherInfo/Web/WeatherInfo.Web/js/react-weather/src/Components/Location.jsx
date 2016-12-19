798452d1911ea1e7dfd7e2be25d4d2042879afbe
John Hilts <johnhilts@live.com>
2016-12-19 00:06:47 -0800

    some files I forgot to add

---
+++ WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Components/Location.jsx
@@ -0,0 +1,73 @@
+﻿import React from 'react';
+
+const DeleteButton = (props) => {
+    const removeLocation = (locationData) => {return false;};
+
+    if (props.isEditMode) {
+        return (
+            <span>
+                <button type="button" className="btn btn-danger" onClick={removeLocation.bind(null, props.locationData)}>
+                    <span className="glyphicon glyphicon-minus" aria-hidden="true"></span>
+                </button>
+                &nbsp;
+            </span>
+        )
+    }
+
+    return <span />
+}
+
+const Location = (props) => {
+
+    let isEditMode = false;
+    let unitType = '';
+    let locations = 
+        [
+            {inputName : 'Test 1', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '123 minutes ago', key : 123, },
+            {inputName : 'Test 2', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '124 minutes ago', key : 124, },
+            {inputName : 'Test 3', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '125 minutes ago', key : 125, },
+            {inputName : 'Test 4', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '126 minutes ago', key : 126, },
+        ];
+    locations.push(props.weatherData[0]);
+    const setEditMode = (isEditMode) => {return false;};
+    const cancelEdit = isEditMode ? <span><a onClick={setEditMode.bind(null, !isEditMode)} className="setEditMode">Cancel Edit</a><br /><br /></span> : <span />
+    let isLoading = false;
+    let loading = isLoading ? <span>loading...</span> : <span />
+    const isIos = () => {return false;};
+    let showGetMore = false;
+    let getMoreDataPrompt = (isIos() && !isLoading) ? <span>Pull up to get more data</span> : <span />
+    const getLocations = () => {return false;};
+    let getMoreLocationsPrompt = (showGetMore && !isIos() && !isLoading) ? <a onClick={getLocations}>... get more ...</a> : <span />
+    let temperature = '80';
+
+    const listOfLocations = (locationData) => {
+        return (
+            <li className="list-group-item" key={locationData.key}>
+                <span className="badge"><span>{temperature}</span>&deg; <span>{unitType}</span></span>
+                <DeleteButton isEditMode={isEditMode} locations={locations} />
+                <span>{locationData.inputName} ({locationData.city}), {locationData.state}&nbsp;{locationData.country}</span>
+                <br /><span>(as of {locationData.temperatureTimeText})</span>
+            </li>
+        )
+    }
+
+    return (
+            <div>
+                <input type="hidden" data-ng-model="unitType" />
+                <div>
+                    {cancelEdit}
+                    <ul className="list-group">
+                        {locations.map(listOfLocations)}
+                    </ul>
+                    {cancelEdit}
+                    {loading}
+                    {getMoreDataPrompt}
+                    {getMoreLocationsPrompt}
+                </div>
+            </div>
+        )
+}
+
+export default Location;
+
+
---
+++ WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Containers/LocationContainer.jsx
@@ -0,0 +1,88 @@
+﻿import React, { Component } from 'react';
+import Location from '../Components/Location';
+import axios from 'axios';
+
+class LocationContainer extends Component {
+    /*
+    constructor() {
+        super();
+    }
+    */
+
+    state = {latitude: '34.18083920000000', longitude: '-118.30896610000002', weatherData: [], };
+
+    // NOTE: componentDidMount is used to initialize a component with server-side info
+    // fore more info, see react docs: https://facebook.github.io/react/docs/component-specs.html
+    componentWillMount() {
+        
+        console.log('componentDidMount');
+
+        const getCurrentWeather = (latitude, longitude) => {
+            return axios.get('https://192.168.1.18/api/Weather?latitude=' + latitude + '&longitude=' + longitude)
+        }
+
+        const processCurrentWeather = (result) => {
+            if (result) {
+                let weatherData = {
+                    currentTemperature: result.data.CurrentTemperature,
+                    currentUnitType: result.data.UnitType,
+                    temperatureTimeText: result.data.TemperatureTimeText,
+                    weatherQueryTime: result.data.WeatherQueryTime,
+                    key: 976,
+                };
+                let weatherDataList = this.state.weatherData;
+                weatherDataList.push(weatherData);
+                this.setState({weatherData: weatherDataList});
+            }
+        }
+
+        return getCurrentWeather(this.state.latitude, this.state.longitude)
+			.then(processCurrentWeather)
+			.catch(function(err) {console.warn('Error in getCurrentWeather: ', err)})
+    }
+
+    render() {
+        // adding a bunch of temporary declarations
+        let addSuccess = false;
+        let addFail = false;
+
+        let showModal = false;
+
+        let modal = showModal ?
+                <div modal="showModal" close="modalCancel()">
+                    <div className="col-md-3"></div>
+                    <add-location className="col-md-6"></add-location>
+                    <div className="col-md-3"></div>
+                </div>
+            : <span />
+
+        const addLocationAlertReset = () => {return false;};
+            let addSuccessAlert = addSuccess ?
+                    <div className="alert alert-success" role="alert">
+                        <button type="button" className="close" aria-label="Close" onClick={addLocationAlertReset}><span aria-hidden="true">&times;</span></button>
+                        Location saved.
+                    </div>
+                : <span />
+
+        let addFailAlert = addFail ?
+                <div className="alert alert-danger" role="alert">
+                    <button type="button" className="close" aria-label="Close" onClick={addLocationAlertReset}><span aria-hidden="true">&times;</span></button>
+                    Location save failed.
+                </div>
+                : <span />
+
+        return (
+        <div>
+            {modal}
+
+            {addSuccessAlert}
+
+            {addFailAlert}
+
+            <Location {...this.props} weatherData={this.state.weatherData} />
+        </div>
+      )
+  }
+}
+
+export default LocationContainer;
