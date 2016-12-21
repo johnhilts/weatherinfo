e2eca56f9ab85f0c169786c94300f9fb0153bc32
John Hilts <johnhilts@live.com>
2016-12-20 16:02:19 -0800

    more and more functionality converted over

--- WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Components/Location.jsx
+++ WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Components/Location.jsx
@@ -1,6 +1,8 @@
 ﻿import React from 'react';
 
 const DeleteButton = (props) => {
+
+
     const removeLocation = (locationData) => {return false;};
 
     if (props.isEditMode) {
@@ -19,34 +21,28 @@ const DeleteButton = (props) => {
 
 const Location = (props) => {
 
+    if (props.isLoading) {
+        return (<span>Loading ...</span>)
+    }
+
     let isEditMode = false;
     let unitType = '';
-    let locations = 
-        [
-            {inputName : 'Test 1', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '123 minutes ago', key : 123, },
-            {inputName : 'Test 2', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '124 minutes ago', key : 124, },
-            {inputName : 'Test 3', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '125 minutes ago', key : 125, },
-            {inputName : 'Test 4', city : 'Testville', state : 'NV', country : 'US', temperatureTimeText : '126 minutes ago', key : 126, },
-        ];
-    if (props.locations && props.locations[0]) {
-        locations.push(props.locations[0]);
-    }
     const setEditMode = (isEditMode) => {return false;};
     const cancelEdit = isEditMode ? <span><a onClick={setEditMode.bind(null, !isEditMode)} className="setEditMode">Cancel Edit</a><br /><br /></span> : <span />
     let isLoading = false;
     let loading = isLoading ? <span>loading...</span> : <span />
+
     const isIos = () => {return false;};
     let showGetMore = false;
     let getMoreDataPrompt = (isIos() && !isLoading) ? <span>Pull up to get more data</span> : <span />
     const getLocations = () => {return false;};
     let getMoreLocationsPrompt = (showGetMore && !isIos() && !isLoading) ? <a onClick={getLocations}>... get more ...</a> : <span />
-    let temperature = '80';
 
     const listOfLocations = (locationData, index) => {
         return (
             <li className="list-group-item" key={index}>
-                <span className="badge"><span>{temperature}</span>&deg; <span>{unitType}</span></span>
-                <DeleteButton isEditMode={isEditMode} locations={locations} />
+                <span className="badge"><span>{locationData.currentTemperature}</span>&deg; <span>{locationData.currentUnitType}</span></span>
+                <DeleteButton isEditMode={isEditMode} locations={props.locations} />
                 <span>{locationData.inputName} ({locationData.city}), {locationData.state}&nbsp;{locationData.country}</span>
                 <br /><span>(as of {locationData.temperatureTimeText})</span>
             </li>
@@ -54,20 +50,20 @@ const Location = (props) => {
     }
 
     return (
+        <div>
+            <input type="hidden" data-ng-model="unitType" />
             <div>
-                <input type="hidden" data-ng-model="unitType" />
-                <div>
-                    {cancelEdit}
-                    <ul className="list-group">
-                        {locations.map(listOfLocations)}
-                    </ul>
-                    {cancelEdit}
-                    {loading}
-                    {getMoreDataPrompt}
-                    {getMoreLocationsPrompt}
-                </div>
+                {cancelEdit}
+                <ul className="list-group">
+                    {props.locations.map(listOfLocations)}
+                </ul>
+                {cancelEdit}
+                {loading}
+                {getMoreDataPrompt}
+                {getMoreLocationsPrompt}
             </div>
-        )
+        </div>
+    )
 }
 
 export default Location;
--- WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Containers/LocationContainer.jsx
+++ WeatherInfo/Web/WeatherInfo.Web/js/react-weather/src/Containers/LocationContainer.jsx
@@ -10,41 +10,92 @@ class LocationContainer extends Component {
         this.setShowMoreButton = this.setShowMoreButton.bind(this);
         this.handleScroll = this.handleScroll.bind(this);
         this.getCurrentLocation = this.getCurrentLocation.bind(this);
+        this.getLocations = this.getLocations.bind(this);
+        this.getCurrentWeather = this.getCurrentWeather.bind(this);
+        this.updateQueryTimeText = this.updateQueryTimeText.bind(this);
     
         let previousUnitType = this.getPreviousUnitType("current");
         let unitType = 'F';
         if (previousUnitType) {
             unitType = this.getPreviousUnitType("current");
         }
-    
-        this.state = {latitude: '34.18083920000000', longitude: '-118.30896610000002', locations: [], unitType: unitType, previousUnitType: previousUnitType, };
+
+        // NOTE: temporarily turning this off because it interferes with hot reload
+        // window.setInterval(this.updateQueryTimeText, 10000);
+
+        this.state = {locations: [], unitType: unitType, previousUnitType: previousUnitType, 
+            domain: '192.168.0.172', showModal: false, addFail: false, indexes: {previousSortIndex: -1, }, showGetMore: false, isLoading: true, isEditMode: false, };
     }
 
     // NOTE: componentDidMount is used to initialize a component with server-side info
     // fore more info, see react docs: https://facebook.github.io/react/docs/component-specs.html
     componentDidMount() {
         this.getCurrentLocation();
+        this.getLocations(this.setShowMoreButton);
 
         window.addEventListener('scroll', this.handleScroll);
     }
 
     getCurrentLocation() {
-        let isCurrent = true;
         const setAddressCallback = (locationData) => {
-            let insertIndex = isCurrent ? 0 : 1;
+            let insertIndex = 0; // isCurrent ? 0 : 1;
             let locations = this.state.locations;
             locations.splice(insertIndex, 0, 
-                { inputName: locationData.inputName, city: locationData.city, state: locationData.stateCode, country: locationData.countryCode, 
+                { inputName: "Current Location", city: locationData.city, state: locationData.stateCode, country: locationData.countryCode, 
                     latitude: locationData.latitude, longitude: locationData.longitude, });
-            this.setState({locations: locations, });
+            this.setState({locations: locations, }, 
+                this.getCurrentWeather(this.state.locations[0].latitude, this.state.locations[0].longitude, 0));
         }
 
         geoLocation.getCurrentLocation(setAddressCallback);
     }
 
-    notSureWhereToPutThis() {
-        const getCurrentWeather = (latitude, longitude) => {
-            return axios.get('https://192.168.1.18/api/Weather?latitude=' + latitude + '&longitude=' + longitude)
+    getLocations(successCallback) {
+
+        // $scope.startFeedback();
+
+        const queryLocations = (previousSortIndex) => {
+            return axios.get(`https://${this.state.domain}/api/Location?previousSortOrder=${previousSortIndex}`)
+        }
+
+        const processLocations = (result) => {
+            if (result) {
+
+                const setLocations = (locationData) => { // HACK
+                    // TODO: change to locations.push(location) - we will need to make the property names the same
+                    this.state.locations.push(
+                        { inputName: locationData.InputName, city: locationData.City, state: locationData.StateCode, country: locationData.CountryCode, 
+                            latitude: locationData.Latitude, longitude: locationData.Longitude, sortOrder: locationData.sortOrder, temperatureTimeText: "...", });
+                    if (this.state.indexes.previousSortIndex == -1 || locationData.SortOrder < this.state.indexes.previousSortIndex) {
+                        this.state.indexes.previousSortIndex = locationData.SortOrder;
+                    }
+                }
+
+                for (let i = 0; i < result.data.length; i++) {
+                    setLocations(result.data[i]);
+                }
+            }
+
+            const getWeatherForLocation = (locationData, locationIndex) => {
+                this.getCurrentWeather(locationData.latitude, locationData.longitude, locationIndex);
+            }
+
+            const getWeatherForAllLocations = () => {
+                this.state.locations.map(getWeatherForLocation);
+                this.setState({isLoading: false, });
+            }
+
+            this.setState({locations: this.state.locations, }, getWeatherForAllLocations);
+        }
+
+        queryLocations(this.state.indexes.previousSortIndex)
+			.then(processLocations)
+			.catch(function(err) {console.warn('Error in queryLocations: ', err)})
+    }
+
+    getCurrentWeather(latitude, longitude, locationIndex) {
+        const queryCurrentWeather = (latitude, longitude) => {
+            return axios.get(`https://${this.state.domain}/api/Weather?latitude=${latitude}&longitude=${longitude}`)
         }
 
         const processCurrentWeather = (result) => {
@@ -54,18 +105,40 @@ class LocationContainer extends Component {
                     currentUnitType: result.data.UnitType,
                     temperatureTimeText: result.data.TemperatureTimeText,
                     weatherQueryTime: result.data.WeatherQueryTime,
-                    key: 976,
                 };
-                // note: got rid of weatherData, using locations instead ... so need to fix this
-                let weatherDataList = this.state.weatherData;
-                weatherDataList.push(weatherData);
-                this.setState({weatherData: weatherDataList});
+                this.state.locations[locationIndex].currentTemperature = weatherData.currentTemperature;
+                this.state.locations[locationIndex].currentUnitType = weatherData.currentUnitType;
+                this.state.locations[locationIndex].temperatureTimeText = weatherData.temperatureTimeText;
+                this.state.locations[locationIndex].weatherQueryTime = weatherData.weatherQueryTime;
+                this.setState({locations: this.state.locations, });
             }
         }
 
-        return getCurrentWeather(this.state.latitude, this.state.longitude)
+        queryCurrentWeather(latitude, longitude)
 			.then(processCurrentWeather)
-			.catch(function(err) {console.warn('Error in getCurrentWeather: ', err)})
+			.catch(function(err) {console.warn('Error in queryCurrentWeather: ', err)})
+    }
+
+    updateQueryTimeText() {
+        
+        const getQueryTimeText = (queryTime) => {
+            return axios.get(`https://${this.state.domain}/api/Common/QueryTimeText?queryTime=${queryTime}`)
+        }
+
+        const getQueryTimeTextSuccess = (i, result) => {
+            this.state.locations[i].temperatureTimeText = result.data;
+            this.setState({locations: this.state.locations, });
+        }
+
+        for (let i = 0; i < this.state.locations.length; i++) {
+            getQueryTimeText(this.state.locations[i].weatherQueryTime)
+            .then(getQueryTimeTextSuccess.bind(null, i))
+            .catch(
+                function () {
+                    // don't do anything ... it's just a text update, not important if it fails
+                }
+            );
+        }
     }
 
     componentWillUnmount() {
@@ -131,7 +204,7 @@ class LocationContainer extends Component {
 
             {addFailAlert}
 
-            <Location {...this.props} locations={this.state.locations} />
+            <Location {...this.props} locations={this.state.locations} isLoading={this.state.isLoading} />
         </div>
       )
   }
